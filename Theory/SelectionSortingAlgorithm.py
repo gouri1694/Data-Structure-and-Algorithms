6af6fac4cca68957730960bd428d7d8c6662f0cc
Gouri Patil <37696826+gouri1694@users.noreply.github.com>
2022-07-28 19:38:45 +0530

    Update SelectionSortingAlgorithm.py

--- Theory/SelectionSortingAlgorithm.py
+++ Theory/SelectionSortingAlgorithm.py
@@ -2,8 +2,11 @@
 # Comparison: Quick, merge,Bubble, Selection, Insertion, Heap; Minimum time complexity: O(nlogn)
 # Non-comparison based: Counting, Bucket, Radix; Minimum time complexity: O(n)
 # O(n)< O(nlogn)
-# function to sort an array of integers using selection sort (O(n^2)
+
+# function to sort an array of integers using selection sort (O(n^2))
+
 def selection_sort(arr):
+    # The idea is to loop through the array and find the minimum element and swap it with the current element.
     # loop through the array
     for i in range(len(arr)):
         # set the minimum to the current index
@@ -18,3 +21,8 @@ def selection_sort(arr):
         arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 print(selection_sort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0]))
+
+# Whenever minimum number of swaps is required, we can use selection sort(better than bubble sort).
+# Number of comparisons in selection sort:O(n^2) [after each 1 element is less(ex: 1st pass n-1, 2nd pass n-2, so on..), n-1+n-2+...3+2+1=n(n-1)/2]
+# Number of swaps in selection sort:O(n) [after each pass 1 swap is done, so its 1+1+1...+1 till (n-1)swaps are done])]
+# Time complexity: O(n^2)
